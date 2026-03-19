e3d35a9b2b34e9e4dde9b694e7d55594cf59bc33
Gustavo Schmitt <gustavo62210416@alunos.sc.senac.br>
2026-03-19 20:40:42 -0300

    Resposta da atividade 6 da segunda lista

---
+++ atividade6.py
@@ -0,0 +1,7 @@
+nome = input('Digite o nome do aluno: ')
+idade = int(input('Digite a idade do aluno: '))
+aluno = {'nome': nome, 'idade': idade}
+print(aluno)
+print('O nome do aluno é',aluno['nome'])
+print("A idade do aluno é", aluno['idade'])
+print(type(aluno))
