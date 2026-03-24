a82bee548ef26357eddcf761ce0055533a5cfb59
Gustavo Schmitt <gustavo62210416@alunos.sc.senac.br>
2026-03-24 19:57:03 -0300

    Resposta da atividade 15 da segunda lista de atividades

---
+++ atividade15.py
@@ -0,0 +1,8 @@
+dias = ('segunda', 'terca', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo')
+print(dias)
+
+print('o primeiro dia da semana é:', dias[0])
+
+print('O ultimo dia da semana é:', dias[-1])
+
+print('O tamanho da tupla é:', len(dias))
