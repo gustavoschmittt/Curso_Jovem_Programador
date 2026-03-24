4400c018675ee9085b2b4283ca076dee4445e51c
Gustavo Schmitt <gustavo62210416@alunos.sc.senac.br>
2026-03-24 19:52:13 -0300

    Resposta da atividade 14 da segunda lista de atividades

---
+++ atividade14.py
@@ -0,0 +1,8 @@
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite um numero: '))
+num3 = int(input('Digite um numero: '))
+num4 = int(input('Digite um numero: '))
+numeros = (num1, num2, num3, num4)
+
+print('O menor numero é:', min(numeros))
+print('O maior numero é:', max(numeros))
