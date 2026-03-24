6a1c09e774796a9dcddf3af471607508a6d756b1
Gustavo Schmitt <gustavo62210416@alunos.sc.senac.br>
2026-03-24 19:33:04 -0300

    Resposta da atividade 12 da segunda lista de atividade

---
+++ atividade12.py
@@ -0,0 +1,7 @@
+fruta1 = input('Digite uma fruta: ')
+fruta2 = input('Digite uma fruta: ')
+fruta3 = input('Digite uma fruta: ')
+frutas = (fruta1, fruta2, fruta3)
+fruta4 = input('Digite uma fruta: ')
+
+print('A chave:',fruta4,' existe?', fruta4 in frutas )
